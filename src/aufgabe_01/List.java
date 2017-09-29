3b804a4a128ec5479e8b1734727840f090b6a45e
Philip Scheer <philip@philip-scheer.de>
2017-09-29 20:37:42 +0200

    List Interface die erste.

---
+++ src/aufgabe_01/List.java
@@ -0,0 +1,64 @@
+package aufgabe_01;
+
+/**
+ * Interface for diffrent Implentations
+ * @author Philip Scheer
+ * @version 1.0 - 28.09.2017
+ *
+ */
+public interface List<T> {
+
+	/**
+	 * Adds a new element to the list
+	 * @param elem
+	 * @return index where the element is stored
+	 */
+	public int add(T elem);
+	
+	/**
+	 * Delete a element at a specific index
+	 * @param index
+	 */
+	public void delete(int index);
+	
+	/**
+	 * Deleted a element with a specific key
+	 * @param key
+	 */
+	public void delete(T key);
+	
+	/**
+	 * Find a value in the list
+	 * @param key
+	 * @return return index of the the element or zero if no element was found
+	 */
+	public int find(int key);
+	
+	/**
+	 * Retrieves a value from the list
+	 * @param index
+	 * @return the element
+	 */
+	public T retrieve(int index);
+
+	/**
+	 * Puts out all data of the list in the console
+	 */
+	public void showall();
+	
+	/**
+	 * 
+	 * @return the number of elements
+	 */
+	public int size();
+	
+	
+	/**
+	 * Unify two lists together
+	 * @param s one list
+	 * @param t another list
+	 * @return the new complete List
+	 */
+	public List<T> unify(List<T> s, List<T> t);
+	
+}
