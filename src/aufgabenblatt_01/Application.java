86dfa2431a60e1b4688143090663b134e4f99b73
Sentler <Dennis.Sentler@haw-hamburg.de>
2017-10-17 16:47:02 +0200

    erste performance tests

--- src/aufgabenblatt_01/Application.java
+++ src/aufgabenblatt_01/Application.java
@@ -2,22 +2,48 @@ package aufgabenblatt_01;
 
 public class Application {
 
-	
+	private static double[] listSize = {10e1, 10e2, 10e3, 10e4, 10e5};
 	/**
 	 * 10^k testfälle k= 1-5
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		dataArrayList();
+	}
+	
+	
+	public static void dataArrayList() {
 
-		Counter counter = new Counter();
-		ArrayList<Elem<Integer>> arrayList = new ArrayList<Elem<Integer>>(counter);
-		
-		for(int i = 0; i < 100; i++) {
-			arrayList.add(new Elem<Integer>(30, new Key(i)));
+		for(int i = 0; i < listSize.length; i++) {
+			int bestCase = Integer.MAX_VALUE;
+			int worstCase = 0;
+			
+			for(int j = 0; j < listSize[i]; j++) {
+				Counter counter = new Counter();
+				ArrayList<Elem<Integer>> arrayList = new ArrayList<Elem<Integer>>(counter);
+				
+				for(int h = 0; h < listSize[i]; h++) {
+					arrayList.add(new Elem<Integer>(1, new Key(h)));
+				}
+				
+				counter.reset();
+				arrayList.deleteKey(new Key(j));
+				if(counter.getCount() < bestCase) {
+					bestCase = (int) counter.getCount();
+				}
+				
+				if(counter.getCount() > worstCase) {
+					worstCase = (int) counter.getCount();
+				}
+				
+				
+			}
+			System.out.println(listSize[i]+" - BEST: "+bestCase);
+			System.out.println(listSize[i]+" - WORST: "+worstCase);
+			
 		}
 		
-		System.out.println("Count: "+counter.getCount());
 	}
 
 }
