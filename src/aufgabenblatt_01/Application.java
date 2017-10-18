48fdc0de6e175b314163efa22536fc8c9298e7a5
DennisSentler <dennis.sentler@haw.hamburg.de>
2017-10-18 15:22:09 +0200

    Vergleich von DLSet und ArrayList

--- src/aufgabenblatt_01/Application.java
+++ src/aufgabenblatt_01/Application.java
@@ -9,6 +9,17 @@ public class Application {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("ArrayList");
+		performanceCompareArrayList();
+		System.out.println("DoubleLinkedSet");
+		performanceCompareDoubleLinkedSet();
+		
+		
+		
+	}
+
+	private static void performanceCompareArrayList() {
 		System.out.println("DeleteKey:");
 		System.out.println("10e1");
 		performanceArrayTestDeleteKey10powX(1);
@@ -32,9 +43,6 @@ public class Application {
 		performanceArrayTestDeletePos10powX(4);
 		System.out.println("10e5");
 		performanceArrayTestDeletePos10powX(5);
-		
-		
-		
 	}
 	
 	public static void performanceArrayTestDeleteKey10powX(int pow)
@@ -70,6 +78,67 @@ public class Application {
 		arrayList.deletePos(new Pos<Integer>(testrange-1, true));
 		System.out.println("  Letztes Element: "+arrayList.counter.getCount());
 	}
+	
+	private static void performanceCompareDoubleLinkedSet() {
+		System.out.println("DeleteKey:");
+		System.out.println("10e1");
+		performanceDLTestDeleteKey10powX(1);
+		System.out.println("10e2");
+		performanceDLTestDeleteKey10powX(2);
+		System.out.println("10e3");
+		performanceDLTestDeleteKey10powX(3);
+		System.out.println("10e4");
+		performanceDLTestDeleteKey10powX(4);
+		System.out.println("10e5");
+		performanceDLTestDeleteKey10powX(5);
+		
+		System.out.println("DeletePos:");
+		System.out.println("10e1");
+		performanceDLTestDeletePos10powX(1);
+		System.out.println("10e2");
+		performanceDLTestDeletePos10powX(2);
+		System.out.println("10e3");
+		performanceDLTestDeletePos10powX(3);
+		System.out.println("10e4");
+		performanceDLTestDeletePos10powX(4);
+		System.out.println("10e5");
+		performanceDLTestDeletePos10powX(5);
+	}
+	
+	public static void performanceDLTestDeleteKey10powX(int pow)
+	{
+		int testrange = (int)Math.pow(10, pow);
+		
+		DoubleLinkedSet<Elem<Integer>> dlSet = newDoubleLinkedSet(testrange);
+		dlSet.deleteKey(new Key(0));
+		System.out.println("  Erstes Element: "+dlSet.counter.getCount());
+
+		dlSet = newDoubleLinkedSet(testrange);
+		dlSet.deleteKey(new Key(testrange/2));
+		System.out.println("  Mittleres Element: "+dlSet.counter.getCount());
+		
+		dlSet = newDoubleLinkedSet(testrange);
+		dlSet.deleteKey(new Key(testrange-1));
+		System.out.println("  Letztes Element: "+dlSet.counter.getCount());
+	}
+
+	public static void performanceDLTestDeletePos10powX(int pow)
+	{
+		int testrange = (int)Math.pow(10, pow);
+
+		DoubleLinkedSet<Elem<Integer>> dlSet = newDoubleLinkedSet(testrange);
+		dlSet.deletePos(new Pos<Integer>(1, true));
+		System.out.println("  Erstes Element: "+dlSet.counter.getCount());
+
+		dlSet = newDoubleLinkedSet(testrange);
+		dlSet.deletePos(new Pos<Integer>(testrange/2, true));
+		System.out.println("  Mittleres Element: "+dlSet.counter.getCount());
+		
+		dlSet = newDoubleLinkedSet(testrange);
+		dlSet.deletePos(new Pos<Integer>(testrange, true));
+		System.out.println("  Letztes Element: "+dlSet.counter.getCount());
+	}
+	
 	private static ArrayList<Elem<Integer>> newArray(int size) {
 		Counter counter = new Counter();
 		ArrayList<Elem<Integer>> arrayList = new ArrayList<Elem<Integer>>(counter);
@@ -80,6 +149,18 @@ public class Application {
 		}
 		counter.reset();
 		return arrayList;
+	}
+	
+	private static DoubleLinkedSet<Elem<Integer>> newDoubleLinkedSet(int size) {
+		Counter counter = new Counter();
+		DoubleLinkedSet<Elem<Integer>> dlSet = new DoubleLinkedSet<Elem<Integer>>(counter);
+		
+		
+		for(int h = 0; h < size; h++) {
+			dlSet.add(new Elem<Integer>(1, new Key(h)));
+		}
+		counter.reset();
+		return dlSet;
 	}
 	
 	public static void dataArrayList() {
